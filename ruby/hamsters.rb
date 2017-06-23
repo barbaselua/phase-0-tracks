2bd42f5c752539acfa86273682b2765a5e99a800
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-06-22 20:44:09 -0500

    Add volume level.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -3,3 +3,14 @@ name = gets.chomp
 if name == ''
 	puts "Fluffy"
 end
+puts "Volume level?"
+user_input = false
+until user_input == true
+volume = gets.chomp.to_i
+	if volume >= 10
+		puts "Choose from 1 to 10."
+		user_input = false
+	else 
+		user_input = true
+	end
+end 
