31d8679caa79f77916b50b32b14585fb8183d04f
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-06-26 08:15:35 -0500

    Add pseudocode and create methods encrypt and decrypt

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,27 @@
+# Use the indexed letters from the string to allow us to us the ".next" method to move every letter forward one.
+def encrypt(string_e)
+  index = 0
+  while index < string_e.length
+  z_score = string_e.index"z"
+    if z_score != nil
+      string_e[index] = "a"
+      index += 1
+    else
+      string_e[index] = string_e[index].next!
+      index += 1
+    end
+  end
+  puts string_e
+end
+#Use an alphabet string to determine an input letters location, using index loops to work through input words. Then use that location to move the input letter back a letter.
+def decrypt(string_d)
+  alpha = "abcdefghijklmnopqrstuvwxyz"
+  index = 0
+  while index < string_d.length
+    working_letter = string_d[index]
+    working_index = alpha.index(working_letter)-1
+    string_d[index] = alpha[working_index]
+  index += 1
+  end
+  puts string_d
+end
