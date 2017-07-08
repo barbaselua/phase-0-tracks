5cc4601aaee6eb15a6f80db8b7b5d98fe8e8e238
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-07 22:57:38 -0500

    Create nested data structure.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,47 @@
+highways = {
+    'I-290': {
+        name: 'Eisenhower',
+        traffic_status: 'heavy',
+        lines: {
+          'line_one': {
+            cars: 80,
+            moving_speed: '15/mph'
+          },
+          'line_two': {
+            cars: '110',
+            moving_speed: '0/mph'
+          },
+          'line_three': {
+              cars: 100,
+              speed: '10/mph',
+              passengers_in_fiftieth_car: [
+                'Dan',
+                'Ana',
+                'Steve']
+            }
+          },
+        construction_site: 'yes'
+    },
+    'I-94': {
+      name: 'Edens Expressway',
+      number_of_lines: 4,
+      traffic_status: 'normal',
+      construction_site: 'yes'
+    },
+  'I-90': {
+      name: 'Skyway',
+      number_of_lines: '4',
+      traffic_status: 'heavy',
+      construction_site: 'no'
+    },
+  'I-190': {
+      name: 'Ohare Extension',
+      number_of_lines: 2,
+      traffic_info: {
+        status: 'heavy',
+        cause: 'accident',
+       lines_moving: 1, 
+       lines_blocked: 1
+      }
+    } 
+}
