a27488d12c630f451d96326df8debd06f2ec50c2
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-07 19:01:31 -0500

    Create pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,3 @@
+=begin 
+create an empty hash, prompt the designes/user for client's info and store the info in the hash. print the hash. ask the designer if any update is needed. if no update needed, no action required; if update needed, allow the designer to update any key. store the update in the hash. print the updated hash and exit the program.
+=end 
