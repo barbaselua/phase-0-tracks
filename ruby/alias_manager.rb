a4ffcba8876972612e0e5ee054cf1b9d14010d15
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-10 07:37:31 -0500

    Create pseudocode.

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,16 @@
+# Alias Manager 
+# swapp the first and last name.
+# if a letter is a vowel, change it to the next vowel in the alphabet.
+# if a letter is a consonant, change it to the next consonant in the alphabet.
+# provide user interface
+# store fake names in to a data structure.
+# iterate through the data structure and print the data the user entered.
+#real_name = "Felicia Torres"
+#solution = "Vussit Gimodoe"
+#alphabet = "abcdefghijklmnoprstuvwxyz"????
+#vowels = "aeiou"
+#consonants = "bcdfghjklmnprstvwxyz"
+#real_name = "Felicia Torres"
+#fake_name = "Vussit Gimodoe"
+# first method will take the name, convert the string into an array, reverse the letters of the array, have the reverse
+#name stored into a string
