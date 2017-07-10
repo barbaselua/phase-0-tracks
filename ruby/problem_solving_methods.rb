77f5cca20479101edfea4e650c8e6f29842b11d2
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-10 06:35:29 -0500

    Creata Fibonacci method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -14,3 +14,20 @@ end
 arr = [42, 89, 23, 1]
 p search_arr(arr, 1)
 p search_arr(arr, 5)
+
+#release 1 
+def fibonacci(n)
+  if n ==1 
+    1 
+  elsif n==2 
+    1 
+  else 
+    fibonacci(n-1) + fibonacci(n-2)
+  end 
+end 
+n = 6
+puts "#{n}'s fibonacci value is #{fibonacci(n)}."
+array = [fibonacci(n-2), fibonacci(n-1)]
+array.each do |num| p num
+end 
+# unfortunately our program prints out the only the last 2 Fibonacci terms.
