cf35f2b804c4f0349b485cb13709db5124d41ac1
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-13 14:47:23 -0500

    Add class design to puppy class.

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+# SPECIES ----------
+# My favorite type of Pomsky
+# CHARACTERISTICS ----------
+# Breed: cross-breed
+# Pomeranian inheritance: size
+# Husky inheritance: looks
+# Eye color: blue
+# Fur: gray&white
+# Height: 10'-15'
+# BEHAVIOR ----------
+# Playful
+# Protective
+# Lick
