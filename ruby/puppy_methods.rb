bceaeb8b6eba0e71db5b3141984c81479d214930
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-13 18:41:05 -0500

    Design, implement class Cats, add initialize method and 2 additional methods, verify them.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -14,7 +14,7 @@ class Puppy
   	puts y*7
   end
   def jump
-  	puts "Whenever I jump, it means I am happy to see you"
+  	puts "Whenever I jump, it means I am happy to see you."
   end
   def initialize
   	puts "Initializing new puppy instance ..."
@@ -27,3 +27,21 @@ puts Bud.speak(3)
 puts Bud.roll_over
 puts Bud.dog_years(3)
 puts Bud.jump
+
+class Cats
+
+	def speak(z)
+	puts "Meow! "*z 
+	end
+	def fetch(toy)
+		puts "I did not bring back the #{toy} because I am a cat." 
+	end
+	def initialize
+		puts "I am cat. I don't like people."
+	end 
+
+end
+Kitty = Cats.new
+puts Kitty.speak(5)
+puts Kitty.fetch("cotton mouse")
+
