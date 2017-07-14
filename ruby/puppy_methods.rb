c7a51dbd23ebe37826c4dd9abde95dbd2dd3ab31
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-14 13:57:27 -0500

    Make adjustments to indentation and method calls.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -6,42 +6,48 @@ class Puppy
   end
   def speak(x)
     puts "Woof! "*x
+    x
   end 
   def roll_over
-  	puts "*rolls over*"
+    puts "*rolls over*"
   end 
   def dog_years(y)
-  	puts y*7
+    puts y*7
+    y
   end
   def jump
-  	puts "Whenever I jump, it means I am happy to see you."
+    puts "Whenever I jump, it means I am happy to see you."
   end
   def initialize
-  	puts "Initializing new puppy instance ..."
+    puts "Initializing new puppy instance ..."
   end
 end
   
 Bud = Puppy.new
-puts Bud.fetch('ball')
-puts Bud.speak(3)
-puts Bud.roll_over
-puts Bud.dog_years(3)
-puts Bud.jump
-
+bud.fetch('ball')
+bud.speak(3)
+bud.roll_over
+bud.dog_years(3)
+bud.jump
+puts "Cats class ----------------------------"
 class Cats
-
-	def speak(z)
-	puts "Meow! "*z 
-	end
-	def fetch(toy)
-		puts "I did not bring back the #{toy} because I am a cat." 
-	end
-	def initialize
-		puts "I am cat. I don't like people."
-	end 
-
+  def speak
+    puts "Meow!"
+  end
+  def fetch(toy)
+    puts "Cats don't bring back the #{toy}." 
+    toy	
+  end
+  def initialize
+    puts "I am cat. I don't like people."
+  end 
 end
-Kitty = Cats.new
-puts Kitty.speak(5)
-puts Kitty.fetch("cotton mouse")
+#driver code 
+kitty = Cats.new
+kitty.speak(5)
+kitty.fetch("cotton mouse")
+ 
+
+
+
 
