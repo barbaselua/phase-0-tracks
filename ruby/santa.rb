746d8dd1233351a8216b021ad0838be715be01f4
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-15 19:35:45 -0500

    Create getter methods age and ethnicity.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,16 +5,25 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0
 	end 
+	def age 
+		@age 
+	end
+	def ethnicity
+		@ethnicity
+	end 
 end 
-santas = []
-santas << Santa.new("female", "black")
-santas << Santa.new("male", "asian")
-santas << Santa.new("agender", "Latino")
-santas << Santa.new("N/A", "white")
+# driver code 
+#santas = []
+#santas << Santa.new("female", "black")
+#santas << Santa.new("male", "asian")
+#santas << Santa.new("agender", "Latino")
+#santas << Santa.new("N/A", "white")
+
+	
