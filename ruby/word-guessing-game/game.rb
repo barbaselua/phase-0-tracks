fa2c94ce6d0a5839c91dc97659e20804fae4ca9d
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-17 10:35:01 -0500

    Add pseudocode (had forgotten to copy it from text file).

--- ruby/word-guessing-game/game.rb
+++ ruby/word-guessing-game/game.rb
@@ -1,11 +1,15 @@
+# create class Word Guessing Game 
 class WordGuessingGame
 
+# create initialization method so every instance of a class 
+#has initial data
   def initialize(word)
     @word = word
     @current_state = "_" * word.length
     @win
   end
 
+# add getter methods to make instance variables readable.
   def get_word
     @word 
   end 
@@ -14,6 +18,8 @@ class WordGuessingGame
     @current_state
   end
 
+#create method to have access to user1 input and user2 input
+#in order to work with the information inputted in the driver code
   def guess_letter(letter)
     @word.split('').each_with_index do |char, index|
       if letter == char
@@ -26,6 +32,7 @@ class WordGuessingGame
     end 
   end
 
+#create method that eliminates repeated input from user2
 #does not work
   def unique(character)
     @words.split(''.uniq)
@@ -43,14 +50,16 @@ class WordGuessingGame
 
 end 
 
-#driver code 
+#driver code to test our code 
 puts "You are now playing Word Guessing!"
 puts "Player one, please write a word:"
+# obtaining input from player 1
 word_to_be_guessed = gets.chomp 
 game = WordGuessingGame.new(word_to_be_guessed)
 letters = []
 tries = word_to_be_guessed.length
 puts "Player two, guess a letter."
+#obtaining input from player 2
 guess = gets.chomp 
 game.guess_letter(guess)
 letters << guess
