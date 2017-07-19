770986c523e49908fe9c046ec193688779d139f2
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-19 18:53:46 -0500

    Pseudocode the reverse function.

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+/*create a function that takes a string as a parameter
+	declare an empty variable that will store the reversed string
+	iterate through every letter in the string
+  	set our counter
+   	set the 'while' condition 
+   	increment/decrement out counter
+*/
