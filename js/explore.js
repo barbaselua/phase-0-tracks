c8e75b05a9bf497dbb492b565c63f68fae9eb2af
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-19 20:10:51 -0500

    Add conditional statement to driver code.

--- js/explore.js
+++ js/explore.js
@@ -4,14 +4,43 @@
   	set our counter
    	set the 'while' condition 
    	increment/decrement out counter
+   	return the new string
 */
+
 function reverse(string) {
 	var newString = "";
-	for (var i = string.length - 1; i >=0; i--) {
+	for (var i = string.length - 1; i >= 0; i--) {
 		newString += string[i];
 	}
 	return newString;
 }
-// driver code
+
+// driver code 
 var reversedString = (reverse("hello"));
-console.log(reversedString);
+if (reversedString.length <= 2) {
+	console.log("Not worth the trouble of running this method.");	
+} else {
+	console.log("Here is your reversed word: " + reversedString + ".");
+}
+
+
+/* I misread instruction #5 at first and modified my
+function so that it runs if the length condition is met. 
+after I had already modified it, I reread the instruction
+and realised the condition was supposed to be in the driver
+code. I thought it does no harm to just leave this function
+here.
+function reverse(string) {
+	var newString = "";
+	if (string.length <= 2) {
+		console.log("Not worth the trouble of running this method.")
+	} else {
+		for (var i = string.length - 1; i >=0; i--) {
+			newString += string[i];
+	}
+	console.log(string + " reversed is " + newString + ".")
+	return newString;
+	}
+}
+reverseString("hi");
+*/
