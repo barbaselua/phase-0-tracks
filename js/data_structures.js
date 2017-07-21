92a2892176f42ee2d73c62a23c2085d652ea5cee
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-21 18:50:45 -0500

    Add conditional statement that requests the length of names to be equal to the length of colors.

--- js/data_structures.js
+++ js/data_structures.js
@@ -2,13 +2,20 @@ var colors = ["red", "green", "blue", "white"];
 var names = ["Black Beauty", "Seabuiscuit", "Ed", "Epona"];
 colors.push("orange");
 names.push("Horsey McHorseface");
+//colors.push("green")- if trying to execute this
+// our hash will not be populated because the length of names 
+// and the length of colors are not equal and
+// our program will print out
+// the message in the conditional statement.
 var keysAndValues = [names, colors];
 var horses = {}
-for(var y = 0; y < names.length; y++){
-  for(var i = 0; i < colors.length; i++){
-    horses[names[y]] = colors[y];
-  }
-  keysAndValues.push(horses)
+if (names.length != colors.length) {
+	console.log("Please make sure you are not missing anything.")
+} else {
+	for (var y = 0; y < names.length; y++) {
+		horses[names[y]] = colors[y];
+		keysAndValues.push(horses)
+	}
 }
 console.log(horses);
 
