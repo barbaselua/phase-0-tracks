8040b34260cbd9a829941ce306cfc17091cc1390
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-21 18:58:24 -0500

    Give code structure.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,12 +1,18 @@
+//release 0
+
 var colors = ["red", "green", "blue", "white"];
 var names = ["Black Beauty", "Seabuiscuit", "Ed", "Epona"];
 colors.push("orange");
 names.push("Horsey McHorseface");
-//colors.push("green")- if trying to execute this
-// our hash will not be populated because the length of names 
-// and the length of colors are not equal and
-// our program will print out
-// the message in the conditional statement.
+
+//release 1 
+
+/*colors.push("green")- if trying to execute this after 
+adding the code our hash will not be populated 
+because the length of names 
+and the length of colors are not equal. the program 
+will print out the message in the conditional statement.
+*/
 var keysAndValues = [names, colors];
 var horses = {}
 if (names.length != colors.length) {
@@ -18,6 +24,8 @@ if (names.length != colors.length) {
 	}
 }
 console.log(horses);
+
+//release 2
 
 
 
