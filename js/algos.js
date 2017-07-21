460e9e5ad7b0173a004a354c23fc055dffda1b21
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-21 13:00:58 -0500

    Pseudocode the function for finding the longest word/phrase of an array.

---
+++ js/algos.js
@@ -0,0 +1,14 @@
+/*
+declare a function that takes an array as parameter
+	declare a variable to be used to store the longest element of the array
+	for now the variable will be an empty string	
+	iterate through every element in the array
+		set the counter at 0 
+		set the 'WHILE' condition - the iteration will take place as long as our condition is false
+		increment the counter by 1
+		declare a conditional statement
+			compare the array element with the variable
+			IF the array element is longer than the variable
+			assign the array element to the variable
+	return the variable
+*/
