51fb9b14e6facc77e991dac5c3f24124da2ea36d
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-30 17:57:33 -0500

    Add create table command.

--- databases/sql-and-ruby/some_ruby.rb
+++ databases/sql-and-ruby/some_ruby.rb
@@ -1,3 +1,14 @@
 require 'sqlite3'
 
 db = SQLite3::Database.new("flights.db")
+
+create_table = <<-SQL 
+CREATE TABLE IF NOT EXISTS flights(
+  id INTEGER PRIMARY KEY,
+  company_name VARCHAR(255),
+  destination VARCHAR(255),
+  passenger_no UNSIGNED, 
+  departure_time TIME,
+  clear_for_take_off BOOLEAN
+  )
+SQL
