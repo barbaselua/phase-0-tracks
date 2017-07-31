fcaede3bc378b28d6a1155d244fe60ca00f389ab
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-07-31 00:03:52 -0500

    Define display_flights_info method.

--- databases/sql-and-ruby/some_ruby.rb
+++ databases/sql-and-ruby/some_ruby.rb
@@ -17,7 +17,22 @@ SQL
 db.execute(create_table)
 
 def create_flight(db, company_name, destination, passenger_no, departure_time, clear_for_take_off)
-  db.execute("INSERT INTO flights(company_name, destination, passenger_no, departure_time, clear_for_take_off") 
-  VALUES (?, ?, ?, ?, ?), [company_name, destination, passenger_no, departure_time, clear_for_take_off]
+  db.execute("INSERT INTO flights(company_name, destination, passenger_no, departure_time, clear_for_take_off) 
+  VALUES (?, ?, ?, ?, ?)", [company_name, destination, passenger_no, departure_time, clear_for_take_off])
 end
-
+def display_fligth_info(db)
+  flight_info = db.execute("SELECT * FROM flights")
+  flight_info.each do |flight, details|
+    if flight['clear_for_take_off'] = true
+      puts 'Yes'
+    else
+      puts 'No'
+    end 
+    puts "Flight information:"
+    puts "Company: #{flight['company_name']}\n
+    Destination: #{flight['destination']}\n
+    Number of passengers: #{flight['passenger_no']}\n
+    Departure time: #{flight['departure_time']}\n
+    Ready for take off: #{clear_for_take_off}"
+  end 
+end
