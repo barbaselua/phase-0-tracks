39f2bb12476f9e5d007d2c31e21551b75ec71fd8
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-08-04 11:08:02 -0500

    Add GET route that displays an address.

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,9 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write a GET route that displays an address
+
+get '/contact' do 
+  "123 Main Street, City, State, 12345"
+end 
