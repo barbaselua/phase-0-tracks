eb230213529efec81687f447c8490e4e9957df6c
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-08-04 20:34:10 -0500

    Add pairing work.

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -10,7 +10,13 @@ db.results_as_hash = true
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
+  # @campuses = db.execute("SELECT * FROM campuses")
   erb :home
+end
+
+get '/second' do 
+  @students = db.execute("SELECT * FROM students")
+  erb :second_home
 end
 
 get '/students/new' do
@@ -23,5 +29,15 @@ post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+# we tried to build a form and add a campuses table in order for the user to be able to add adintional cities. 
+# doesn't work /:(
+# get 'campuses/new' do 
+#   erb :campuses
+# end 
+
+# post '/campuses' do 
+#   db.execute("INSERT INTO campuses (cities, capacity) VALUES (?, ?)", [params['cities'], params['capacity'].to_i])
+# end
 
 # add static resources
