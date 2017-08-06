caab794256d2e362abbd3196f9e6ad199a85927d
Andreea Barbaselu <barbaselu.andreea@yahoo.com>
2017-08-06 17:12:02 -0500

    Add code to script.js to hide/reveal a htmal element.

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -3,4 +3,12 @@ jQuery(document).ready (function() {
 	jQuery("#reveal").click (function() {
 		jQuery("#list").slideToggle();
 	});
+	jQuery("#like").click(function() {
+		jQuery("#lizard-photo").hide();
+		alert("Don't like lizards?")
+	});
+	jQuery("#dislike").click(function () {
+		jQuery("#lizard-photo").show();
+		alert("I knew you would come around!")
+	});
 });
